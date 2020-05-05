f1a76893849fc93ed8c5b40016a5c40f31d656aa
justinaiken7 <60617740+justinaiken7@users.noreply.github.com>
2020-05-05 11:46:04 -0400

    Update app.js
    Second case if white rice is entered, then the ingredients for white rice is displayed

--- js/app.js
+++ js/app.js
@@ -8,3 +8,9 @@ case "California Rice":
 how = "<br><h3 style='color:green;'> Ounces </h3><input  type='text' name='txtres' disabled> <h1> <br>How to make California Rice?</h1><h2>For slightly al dente rice:<br> Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.</h2><br><h2>For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.</h2>";
 
 break;
+
+case "White Rice":
+
+    how = "<br><h3 style='color:green;'> Ounces </h3> <input  type='text' name='txtres' disabled> <h1> <br> <h1>How to make white rice?</h1><h2>Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.</h2>";
+    break;
+
